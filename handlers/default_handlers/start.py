a31d78f67491b1588cee941ac4aa1d9ea1f603cb
Grigory Markov <grin_9@mail.ru>
2023-04-25 22:08:25 +0300

    update-bot

---
+++ handlers/default_handlers/start.py
@@ -0,0 +1,16 @@
+from telebot.types import Message
+from keyboards.reply.start import start_keyboard
+from loader import bot
+
+
+@bot.message_handler(commands=["start", "main", '🔙MainMenu'])
+def start_menu(message: Message) -> None:
+
+    if message.text == '/start':
+        answer = f"👋 Hello {message.from_user.first_name}" \
+                 f" I'm test bot.\nI can help you to find a Hotel of your desire!" \
+                 "\nPush [🔎Find Hotel] or type /find to start searching.\n To see all commands" \
+                 " push the button\n[❓Help]\nor enter /help "
+    else:
+        answer = 'You are in main menu'
+    bot.send_message(message.from_user.id, text=answer, reply_markup=start_keyboard())
